63c08c789831db6b80503839595828870f9d8fe9
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-01 17:12:35 +0300

    Add project files.

---
+++ MCD_PersonelUygulamasi/Helper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_PersonelUygulamasi
+{
+    public static class Helper
+    {
+        //Static olarak işareetlenmiş classlar örneklenemez...
+        // Helper H1 = new Helper(); (Hata veriyor)
+
+        
+        /*public void Test()
+        {
+
+        }
+        *
+        *Static olan classlar içerisinde standart metotlar kullanılamaz.
+        *(static olmayan metotlar)
+        */
+
+
+       /* public string Tanim { get; set; }
+        * 
+        * Statik olan classlar içerisinde static olmayan field tanımları yapılamaz.
+        */
+
+
+       /* public Helper()
+        {
+
+        }
+       *
+       *Her nesne örneği alındığında uygulama standart yapıcı metotu çalıştırır.
+       */
+
+        static Helper()
+        {
+            //Uygulama içerisinde istenildği kadar çağırılsınlar sadece ilk çağırılmada 
+            //çalışırlar...
+            Console.WriteLine("Helper => Static class => static CTOR");
+        }
+
+        public static void emailGonder(string aliciEmailAdres,string konu,string icerik)
+        {   
+            //Email gönderme işlemleri devam edecek...
+            Console.WriteLine("Mail gönderim işlemi başarılı.");
+        }
+
+    }
+}
---
+++ MCD_PersonelUygulamasi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_PersonelUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel P1 = new Personel();
+            P1.Isim = "Faik";
+            P1.Soyisim = "Turan";
+            P1.Emailadres = "faik.turan";
+            P1.Cinsiyet = 717770001; //Bay 717770002-Kadın
+
+            Personel P2 = new Personel();
+            P1.Isim = "Ali";
+            P1.Soyisim = "Gel";
+            P1.Emailadres = "ali.gel";
+            P1.Cinsiyet = 717770001;
+
+            Helper.emailGonder("ik@firmam.com", "Yeni Personel Bilgilendirme", P1.Isim + P1.Soyisim);
+            Helper.emailGonder("ik@firmam.com", "Yeni Personel Bilgilendirme", P2.Isim + P2.Soyisim);
+
+            Console.ReadKey();
+
+        }
+    }
+}
